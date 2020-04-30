c0a9f5f7d533f0245fe4f3d9ea407474f7b63b39
Xotonori <xotonori29011991@gmail.com>
2020-04-30 20:16:01 +0300

    Updates

---
+++ precache-manifest.4d65597750574c9723bb4a037c1ed2d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e430c239bd59173c6021437dbad3cb70",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4033e0d97f2ec251ae15",
+    "url": "/portfolio/static/css/main.4967576e.chunk.css"
+  },
+  {
+    "revision": "5715ad830d2c095764f0",
+    "url": "/portfolio/static/js/2.301052fc.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/portfolio/static/js/2.301052fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4033e0d97f2ec251ae15",
+    "url": "/portfolio/static/js/main.20c0d819.chunk.js"
+  },
+  {
+    "revision": "7e84dee687ac6de54091",
+    "url": "/portfolio/static/js/runtime-main.4da689ad.js"
+  },
+  {
+    "revision": "35fcf9dd2a9c81da282a9584d48705da",
+    "url": "/portfolio/static/media/profile.35fcf9dd.jpeg"
+  },
+  {
+    "revision": "f61fe8cd9b6c14356d49abeeaf829873",
+    "url": "/portfolio/static/media/work.f61fe8cd.png"
+  }
+]);
